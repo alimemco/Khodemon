b934ad538f1b2798d4dff912ec52d24da9cd5643
alimemco <alimoghadam75@gmail.com>
2019-02-25 13:33:31 +0330

    Update to Android Studio 3.3

--- app/src/main/java/com/ali/rnp/khodemon/TestActivity.java
+++ app/src/main/java/com/ali/rnp/khodemon/TestActivity.java
@@ -1,58 +1,22 @@
 package com.ali.rnp.khodemon;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
-import com.ali.rnp.khodemon.Views.CustomViews.CustomLoadingDialog;
-import com.roger.catloadinglibrary.CatLoadingView;
+public class TestActivity extends AppCompatActivity {
 
 
-public class TestActivity extends AppCompatActivity implements View.OnClickListener {
-
-    private Button catBtn,customBtn;
-    private CatLoadingView mView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
 
-        mView = new CatLoadingView();
 
-        catBtn = findViewById(R.id.test_btn1);
-        customBtn = findViewById(R.id.testBtn2);
-
-        catBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mView.setText("test");
-                mView.setCanceledOnTouchOutside(true);
-                mView.show(getSupportFragmentManager(),"tag");
-            }
-        });
-
-        customBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                CustomLoadingDialog cdd=new CustomLoadingDialog(TestActivity.this);
-
-                cdd.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-
-                cdd.show();
-            }
-        });
 
     }
 
-    @Override
-    public void onClick(View v) {
 
-    }
 
 
 }
