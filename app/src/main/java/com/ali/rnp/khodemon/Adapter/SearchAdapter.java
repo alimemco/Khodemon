1ffef4b22600c35e7a624ba7acd869269a19b24a
alimemco <alimoghadam75@gmail.com>
2019-06-27 02:08:54 +0430

    FragmentSearch #7 add header to search result

--- app/src/main/java/com/ali/rnp/khodemon/Adapter/SearchAdapter.java
+++ app/src/main/java/com/ali/rnp/khodemon/Adapter/SearchAdapter.java
@@ -21,8 +21,10 @@ import de.hdodenhof.circleimageview.CircleImageView;
 
 public class SearchAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
+    private static int VIEW_TYPE_HEADER = 0 ;
+    private static int VIEW_TYPE_CHILD = 1 ;
     ArrayList<LocationPeople> locationPeopleList;
-    String txt;
+    private String txt;
     Context context;
 
     public SearchAdapter() {
@@ -39,13 +41,29 @@ public class SearchAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
         notifyDataSetChanged();
     }
 
+    @Override
+    public int getItemViewType(int position) {
+
+        return (position==0) ? VIEW_TYPE_HEADER : VIEW_TYPE_CHILD;
+
+    }
 
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.rcv_search, parent, false);
+        if (viewType == VIEW_TYPE_HEADER){
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.rcv_parent, parent, false);
+            return new HeaderHolder(view);
 
-        return new SearchHolder(view);
+        }else  if (viewType == VIEW_TYPE_CHILD){
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.rcv_search, parent, false);
+            return new SearchHolder(view);
+        }else {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.rcv_search, parent, false);
+            return new SearchHolder(view);
+        }
+
+
     }
 
     @Override
@@ -53,14 +71,17 @@ public class SearchAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
 
         if (holder instanceof SearchHolder) {
             SearchHolder mHolder = (SearchHolder) holder;
-            mHolder.bindSearch(mHolder, position);
+            mHolder.bindSearch(mHolder, position-1);
+        }else if (holder instanceof HeaderHolder){
+            HeaderHolder mHolder = (HeaderHolder) holder;
+            mHolder.bindHeader();
         }
 
     }
 
     @Override
     public int getItemCount() {
-        return locationPeopleList == null ? 0 : locationPeopleList.size();
+        return locationPeopleList == null ? 0 : locationPeopleList.size()+1;
     }
 
     class SearchHolder extends RecyclerView.ViewHolder {
@@ -87,6 +108,23 @@ public class SearchAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
                     mHolder.categoryTv.setText(locationPeople.getTag());
 
         }
+    }
+
+    class HeaderHolder extends RecyclerView.ViewHolder {
+        MyTextView headerTitleTv;
+
+         HeaderHolder(@NonNull View itemView) {
+            super(itemView);
+            headerTitleTv = itemView.findViewById(R.id. rcv_parent_title_tv);
+
+        }
+
+        void bindHeader(){
+            int size = locationPeopleList == null ? 0 : locationPeopleList.size();
+            String txt = size + " نتیجه ";
+            headerTitleTv.setText(txt);
+        }
+
     }
 
     @Override
